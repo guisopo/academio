e201978e715b3a0b3689f78e84e11afdaa6f0d2a
guisopo <solerguillermo@hotmail.com>
2021-02-01 17:56:03 +0100

    Import every model and export them in a single object

---
+++ backend/models/index.js
@@ -0,0 +1,13 @@
+const Course = require('./course');
+const Subject = require('./subject');
+const User = require('./user');
+const Quiz = require('./quiz');
+
+const models = {
+  Course,
+  Subject,
+  User,
+  Quiz
+};
+
+module.exports = models;
