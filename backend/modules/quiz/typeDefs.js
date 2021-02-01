26d621094cd032ecabe553e76fd21dbde8df139a
guisopo <solerguillermo@hotmail.com>
2021-02-01 18:30:40 +0100

    Create new file to define GQl types

---
+++ backend/modules/quiz/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server');
+
+module.exports = gql`
+  type Question {
+    question: String,
+    answers: [String!],
+    correct: String
+  }
+
+  type Quiz {
+    id: ID!,
+    title: String!,
+    description: String,
+    questions: [Question]
+  }
+
+  extend type Course {
+    quizzes: [Quiz!]
+  }
+
+  input QuestionInput {
+    question: String,
+    answers: [String!],
+    correct: String
+  }
+
+  input QuizInput {
+    title: String,
+    description: String,
+    questions: [QuestionInput],
+    course: ID,
+  }
+
+  input addQuestionsInput {
+    questions: [QuestionInput]
+  }
+
+  extend type Query {
+    allQuizzes: [Quiz!]!,
+    singleQuiz(id: ID): Quiz,
+  }
+
+  extend type Mutation {
+    createQuiz(args: QuizInput): Quiz,
+    updateQuiz(id: ID, args: QuizInput): Quiz,
+    deleteQuiz(id: ID!): Quiz,
+    addQuestionsToQuiz(id: ID!, args: addQuestionsInput): Quiz
+  }
+`;
