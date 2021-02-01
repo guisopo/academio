cfe4bf89d0e7abfd35154533d3b5327581758202
guisopo <solerguillermo@hotmail.com>
2021-02-01 18:15:17 +0100

    Create new file to define GQl types

---
+++ backend/modules/subject/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require('apollo-server');
+
+module.exports = gql`
+  type Topic {
+    title: String,
+    url: String
+  }
+
+  type Subject {
+    id: ID!,
+    title: String!,
+    description: String,
+    topics: [Topic]
+  }
+
+  extend type Course {
+    subjects: [Subject!]
+  }
+
+  input TopicInput {
+    title: String,
+    url: String
+  }
+
+  input SubjectInput {
+    title: String,
+    description: String,
+    courses: [ID!],
+    topics: [TopicInput]
+  }
+
+  extend type Query {
+    allSubjects: [Subject!]!,
+    singleSubject(id: ID): Subject,
+  }
+
+  extend type Mutation {
+    createSubject(args: SubjectInput): Subject,
+    updateSubject(args: SubjectInput, id: ID): Subject,
+    deleteSubject(id: ID!): Subject
+  }
+`;
