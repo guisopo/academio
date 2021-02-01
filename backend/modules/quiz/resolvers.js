6138284eeab67e720c3efca116c24acb02c27817
guisopo <solerguillermo@hotmail.com>
2021-02-01 18:31:22 +0100

    Create file with auth resolvers

---
+++ backend/modules/quiz/resolvers.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const isAdmin = require('../helpers');
+
+module.exports = {
+  Query: {
+    allQuizzes: async (root, args, { models }) => {
+      try {
+        return models.Quiz.find();
+      } catch (error) {
+        throw error;
+      }
+    },
+
+    singleQuiz : async (root, { id }, { models }) => {
+      try {
+        return models.Quiz.findById(id);
+      } catch (error) {
+        throw(error);
+      }
+    }
+  },
+
+  Mutation: {
+    createQuiz: async (root, { args }, { models, currentUser }) => {
+      try {
+        // Check if user is admin
+        // isAdmin(currentUser);
+        // Create quiz
+        console.log(models);
+        const quiz = await models.Quiz.create({...args} );
+        // Find the course and insert the quiz id
+        const course = await models.Course.findById(args.course);
+        course.quizzes = course.quizzes.concat(quiz.id);
+        await course.save();
+        // Return subject saved in DB
+        return quiz;
+      } catch (error) {
+        throw error;
+      }
+    },
+
+    updateQuiz: async (root, { id, args }, { models, currentUser }) => {
+      // Check if user is admin
+      // isAdmin(currentUser);
+      // Update subject
+      try {
+        return await models.Quiz.findByIdAndUpdate(id, {...args}, { new: true });
+      } catch (error) {
+        throw error;
+      }
+    },
+
+    deleteQuiz: async (root, { id }, { models, currentUser }) => {
+      // Check if user is admin
+      // isAdmin(currentUser);
+      // Delete subject
+      try {
+        return await models.Quiz.findByIdAndRemove(id);
+      } catch (error) {
+        throw error;
+      }
+    },
+
+    addQuestionsToQuiz: async (root, { id, args }, { models, currentUser }) => {
+      // Check if user is admin
+      // isAdmin(currentUser);
+      // Delete subject
+      try {
+        const quiz = await models.Quiz.findById(id);
+        quiz.questions = quiz.questions.concat(args.questions);
+        return await quiz.save();
+      } catch (error) {
+        throw error
+      }
+    }
+  }
+}
